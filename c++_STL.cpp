99c2a31887ffd0d8023676ce6b6edafc11ad52dd
Nilendu Das <nilendu438@gmail.com>
2026-06-20 10:13:45 +0530

    update

--- c++_STL.cpp
+++ c++_STL.cpp
@@ -1,25 +1,23 @@
-#include <bits/stdc++.h> // all standard libraries
+#include <bits/stdc++.h>
 using namespace std;
 
 /*
 =====================================================
-1. FUNCTIONS (Void & Return Type)
+1. FUNCTIONS
 =====================================================
 */
 
-// void function (does not return anything)
 void sayHello() {
-    cout << "Hello from void function\n";
+    cout << "Hello\n";
 }
 
-// return type function
 int add(int a, int b) {
     return a + b;
 }
 
 /*
 =====================================================
-2. CONTAINERS
+2. CONTAINERS (DETAILED)
 =====================================================
 */
 
@@ -29,50 +27,38 @@ void containers_demo() {
     ----------- PAIR -----------
     */
     pair<int, int> p = {1, 2};
-    cout << "Pair: " << p.first << " " << p.second << "\n";
+    cout << p.first << " " << p.second << "\n";
 
-    // nested pair
     pair<int, pair<int, int>> np = {1, {2, 3}};
-    cout << "Nested Pair: " << np.first << " " << np.second.second << "\n";
-
 
     /*
     ----------- VECTOR -----------
     */
-    vector<int> v;
+    vector<int> v = {1, 2, 3};
 
-    v.push_back(10);        // adds element
-    v.emplace_back(20);     // faster than push_back
+    v.push_back(4);
+    v.emplace_back(5);
 
-    vector<int> v2(5, 100); // size 5, all elements = 100
+    cout << "Size: " << v.size() << "\n";
+    cout << "Capacity: " << v.capacity() << "\n";
 
-    vector<int> copyV = v2; // copying vector
+    // insert & erase
+    v.insert(v.begin(), 100);
+    v.erase(v.begin());
 
-    cout << "Vector elements: ";
-    for (int i = 0; i < v2.size(); i++) {
-        cout << v2[i] << " ";
-    }
+    // resize
+    v.resize(10, 0);
+
+    // iteration
+    for (auto it : v) cout << it << " ";
     cout << "\n";
-
-    // iterator
-    cout << "Using iterator: ";
-    for (auto it = v2.begin(); it != v2.end(); it++) {
-        cout << *it << " ";
-    }
-    cout << "\n";
-
 
     /*
-    ----------- LIST (Doubly Linked List) -----------
+    ----------- LIST -----------
     */
     list<int> ls;
     ls.push_back(10);
     ls.push_front(5);
-
-    cout << "List: ";
-    for (auto it : ls) cout << it << " ";
-    cout << "\n";
-
 
     /*
     ----------- DEQUE -----------
@@ -81,212 +67,173 @@ void containers_demo() {
     dq.push_back(10);
     dq.push_front(5);
 
-    cout << "Deque: ";
-    for (auto it : dq) cout << it << " ";
-    cout << "\n";
-
-
     /*
-    ----------- STACK (LIFO) -----------
+    ----------- STACK -----------
     */
     stack<int> st;
-    st.push(10);
-    st.push(20);
-
-    cout << "Stack top: " << st.top() << "\n";
-    st.pop(); // removes top
-
+    st.push(1);
+    st.push(2);
+    st.pop();
 
     /*
-    ----------- QUEUE (FIFO) -----------
+    ----------- QUEUE -----------
     */
     queue<int> q;
-    q.push(10);
-    q.push(20);
-
-    cout << "Queue front: " << q.front() << "\n";
-    cout << "Queue back: " << q.back() << "\n";
+    q.push(1);
+    q.push(2);
     q.pop();
-
 
     /*
     ----------- PRIORITY QUEUE -----------
     */
-    // max heap (default)
-    priority_queue<int> pq;
-    pq.push(10);
-    pq.push(50);
-    pq.push(20);
-
-    cout << "Max Heap top: " << pq.top() << "\n";
+    priority_queue<int> maxpq;
+    maxpq.push(10);
+    maxpq.push(5);
 
     // min heap
     priority_queue<int, vector<int>, greater<int>> minpq;
     minpq.push(10);
-    minpq.push(50);
-    minpq.push(20);
-
-    cout << "Min Heap top: " << minpq.top() << "\n";
-
+    minpq.push(5);
 
     /*
-    ----------- SET (unique + sorted) -----------
+    ----------- SET -----------
     */
-    set<int> s;
-    s.insert(10);
-    s.insert(5);
-    s.insert(10); // duplicate ignored
+    set<int> s = {1, 2, 3};
 
-    cout << "Set: ";
-    for (auto it : s) cout << it << " ";
-    cout << "\n";
+    s.insert(4);
+    s.erase(2);
 
-    cout << "Find 5: " << (s.find(5) != s.end()) << "\n";
-
+    // lower_bound / upper_bound
+    auto lb = s.lower_bound(2);
+    auto ub = s.upper_bound(2);
 
     /*
-    ----------- MULTISET (duplicates allowed) -----------
+    ----------- MULTISET -----------
     */
     multiset<int> ms;
     ms.insert(10);
-    ms.insert(10);
-
-    cout << "Multiset count of 10: " << ms.count(10) << "\n";
-
+    ms.insert(10); // duplicates allowed
 
     /*
     ----------- UNORDERED SET -----------
     */
     unordered_set<int> us;
-    us.insert(10);
-    us.insert(20);
-
-    cout << "Unordered Set: ";
-    for (auto it : us) cout << it << " ";
-    cout << "\n";
-
+    us.insert(1);
 
     /*
-    ----------- MAP (key-value, sorted) -----------
+    ----------- MAP -----------
     */
     map<int, string> mp;
     mp[1] = "one";
     mp[2] = "two";
 
-    cout << "Map: ";
-    for (auto it : mp) {
-        cout << it.first << "->" << it.second << " ";
+    // find
+    if (mp.find(1) != mp.end()) {
+        cout << mp[1] << "\n";
     }
-    cout << "\n";
 
-
-    /*
-    ----------- MULTIMAP -----------
-    */
-    multimap<int, string> mmp;
-    mmp.insert({1, "one"});
-    mmp.insert({1, "uno"});
-
+    // lower_bound
+    auto it = mp.lower_bound(1);
 
     /*
     ----------- UNORDERED MAP -----------
     */
-    unordered_map<int, string> ump;
-    ump[1] = "one";
-    ump[2] = "two";
+    unordered_map<int, int> ump;
+    ump[1] = 100;
 }
-
 
 /*
 =====================================================
-3. ITERATORS
+3. ITERATORS (IMPORTANT CONCEPT)
 =====================================================
 */
 
 void iterator_demo() {
-    vector<int> v = {1, 2, 3, 4};
+    vector<int> v = {1, 2, 3};
 
-    // begin() to end()
+    // normal iterator
     for (auto it = v.begin(); it != v.end(); it++) {
         cout << *it << " ";
     }
     cout << "\n";
 
-    // reverse iteration
+    // reverse iterator
     for (auto it = v.rbegin(); it != v.rend(); it++) {
         cout << *it << " ";
     }
     cout << "\n";
-}
 
+    // reference (no copy)
+    for (auto &it : v) {
+        it += 1;
+    }
+}
 
 /*
 =====================================================
-4. ALGORITHMS
+4. ALGORITHMS (MUST KNOW)
 =====================================================
 */
 
-// custom comparator (descending)
 bool comp(int a, int b) {
-    return a > b;
+    return a > b; // descending
 }
 
 void algorithms_demo() {
 
     vector<int> v = {4, 2, 5, 1, 3};
 
-    /*
-    ----------- SORT -----------
-    */
-    sort(v.begin(), v.end()); // ascending
-    sort(v.begin(), v.end(), comp); // custom (descending)
+    // sort
+    sort(v.begin(), v.end());
+    sort(v.begin(), v.end(), comp);
+
+    // binary search (sorted required)
+    bool found = binary_search(v.begin(), v.end(), 3);
+
+    // lower_bound / upper_bound
+    auto lb = lower_bound(v.begin(), v.end(), 3);
+    auto ub = upper_bound(v.begin(), v.end(), 3);
+
+    // min / max
+    cout << *max_element(v.begin(), v.end()) << "\n";
+    cout << *min_element(v.begin(), v.end()) << "\n";
+
+    // reverse
+    reverse(v.begin(), v.end());
+
+    // count
+    cout << count(v.begin(), v.end(), 3) << "\n";
 
     /*
-    ----------- MIN / MAX -----------
+    ----------- BIT -----------
     */
-    cout << "Max: " << *max_element(v.begin(), v.end()) << "\n";
-    cout << "Min: " << *min_element(v.begin(), v.end()) << "\n";
-
-
-    /*
-    ----------- BIT MANIPULATION -----------
-    */
-    int x = 7; // binary: 111
-    cout << "Set bits: " << __builtin_popcount(x) << "\n";
-
+    int x = 7;
+    cout << __builtin_popcount(x) << "\n";
 
     /*
     ----------- PERMUTATION -----------
     */
     vector<int> perm = {1, 2, 3};
 
-    cout << "Permutations:\n";
     do {
         for (int i : perm) cout << i << " ";
         cout << "\n";
     } while (next_permutation(perm.begin(), perm.end()));
 }
 
-
 /*
 =====================================================
-MAIN FUNCTION
+MAIN
 =====================================================
 */
 
 int main() {
 
-    // functions
     sayHello();
-    cout << "Sum: " << add(5, 3) << "\n";
+    cout << add(2, 3) << "\n";
 
-    // containers
     containers_demo();
-
-    // iterators
     iterator_demo();
-
-    // algorithms
     algorithms_demo();
 
     return 0;
